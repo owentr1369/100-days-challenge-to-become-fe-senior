332e70df02cda006e01b0b1bb280e741816d1805
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-10-08 17:45:28 +0700

    Improve project organization and implement new features for week 2.

---
+++ week-1/day-2/variables.ts
@@ -0,0 +1,12 @@
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(a); // undefined
+var a = 5;
+
+const add = (a, b) => a + b;
+const greet = (userName) => `Hello, ${userName}!`;
+
+const userName = "Tam";
+console.log(`Hi ${userName}, welcome to JS world!`);
